cfe7d50c00842cbdda87eb3d8377013aed875576
aganoza <aganoza@users.noreply.github.com>
2018-12-12 01:43:52 -0500

    día 2 ejercicio 4

--- src/02.ts
+++ src/02.ts
@@ -63,8 +63,103 @@ function calculateChecksum(input: string = readInputFromFile('02')): number {
   return checksum;
 }
 
+// 2nd Part
+
+function calculateCommonLetters(
+  input: string = readInputFromFile('02')
+): string {
+  const IDs: string[] = formatInputString(input);
+  let commonLetters: string = '';
+
+  // for (let id of IDs) {
+  //   console.log(`-> ${id}`);
+  // }
+
+  const commonIdPair = reduceCommonList(/*letterPosition*/ 0, IDs);
+  // console.log(`--> ${commonIdPair}`);
+
+  for (let i = 0; i < commonIdPair[0].length; i++) {
+    if (commonIdPair[0][i] === commonIdPair[1][i]) {
+      commonLetters = commonLetters.concat(commonIdPair[0][i]);
+    }
+  }
+
+  return commonLetters;
+}
+
+// Ir a la letterPosition++ pero solo considerar las ids que tengan los remainingIDsIndexes
+
+function reduceCommonList(letterPosition: number, IDs: string[]): string[] {
+  // Caso base para finalizar recursión
+  if (IDs.length <= 2 && letterPosition !== 0) {
+    return IDs;
+  }
+
+  let lettersOfPos = IDs.map(id => {
+    return id.slice(0, letterPosition + 1);
+  });
+  // console.log(`==> ${letterPosition}: ${lettersOfPos}`);
+
+  // TODO: Cambiar acc a array, no necesita ser objeto, así podríamos hacer filter abajo para obtener remainingIDsIndexes
+  const letterCounter: {
+    [key: string]: { index: number[]; count: number };
+  } = lettersOfPos
+    .map((letter, index) => {
+      return { index, letter, count: 1 };
+    })
+    .reduce(
+      (
+        acc: {
+          [key: string]: { index: number[]; count: number };
+        },
+        value
+      ) => {
+        let newIndexArray = acc[value.letter]
+          ? [...acc[value.letter].index, value.index]
+          : [value.index];
+
+        acc[value.letter] = {
+          index: newIndexArray,
+          count: (acc[value.letter] ? acc[value.letter].count : 0) + value.count
+        };
+
+        return acc;
+      },
+      {}
+    );
+
+  // console.log(JSON.stringify(letterCounter, null, 2));
+
+  // Obtenemos los indices de los subids que hayan coincidido
+
+  let remainingIDsIndexes: number[] = [];
+
+  for (let key in letterCounter) {
+    if (letterCounter[key].count > 1) {
+      let { count, ...rest } = letterCounter[key];
+      remainingIDsIndexes = remainingIDsIndexes.concat(rest.index);
+    }
+  }
+
+  remainingIDsIndexes = remainingIDsIndexes.sort();
+
+  // Solo mantenemos IDs que hayan coincidido para usarlos en la siguiente vuelta
+
+  IDs = IDs.filter((id, index) => {
+    return remainingIDsIndexes.indexOf(index) > -1;
+  });
+
+  // console.log(JSON.stringify(IDs, null, 2));
+  // console.log(JSON.stringify(remainingIDsIndexes, null, 2));
+
+  // Generamos recursión, aumenta la posición de la letra a comparar entre IDs
+  return reduceCommonList(++letterPosition, IDs);
+}
+
 export {
   calculateChecksum,
   countLetterAppearance,
-  getDifferentAppearanceNumbersById
+  getDifferentAppearanceNumbersById,
+  calculateCommonLetters,
+  reduceCommonList
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import * as main from './02';
 
-const result = main.calculateChecksum();
+const result = main
+  .calculateCommonLetters
+  // 'abcde, fghij, klmno, pqrst, fguij, axcye, wvxyz'
+  ();
 console.log(`The result is ${result}`);
