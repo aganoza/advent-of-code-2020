65999dbb0a001c2c3764a43de99e3380ad97fee6
aganoza <aganoza@users.noreply.github.com>
2018-12-13 16:26:32 -0500

    día 3 ejercicio 6

--- src/03.ts
+++ src/03.ts
@@ -1,4 +1,5 @@
 import { formatInputString, readInputFromFile } from './utils';
+import * as fs from 'fs';
 
 /**
  * - The number of inches between the left edge of the fabric and the left edge of the rectangle.
@@ -115,31 +116,63 @@ function calculateOverlappingInches(
 
 // 2da Parte
 
-function getIDNonOverlappingClaim(
-  fabricHeight: number,
-  fabricWidth: number,
-  input: string = readInputFromFile('03')
-): number | null {
-  const claims: string[] = formatInputString(input, /;|\n|\r\n/g);
-  let IDNonOverlappingClaim: number | null = null;
-  let fabric: string[] = createFabric(fabricHeight, fabricWidth);
-  // console.log(fabric);
+type UnicodeValidChar = {
+  claimId: number;
+  hex: string;
+  glyphDescription: string;
+  glyph: string;
+};
 
-  for (const claim of claims) {
-    // console.log(claim);
-    const claimDetail: ClaimDetail = parseClaim(claim);
-    // console.log(claimDetail);
-    const { claimedFabric, isNonOverlapping } = insertClaimInFabricAlternative(
-      fabric,
-      claimDetail
-    );
-    fabric = claimedFabric;
-    IDNonOverlappingClaim = isNonOverlapping ? claimDetail.id : null;
+const unicodeValidChars: UnicodeValidChar[] = getUnicodeValidChars();
 
-    // console.log(fabric);
+// TODO: Limpiar función innecesaria, no se necesita un caracter único para determinar
+// si hay o no sobreposición, mirar método isClaimNonOverlapping
+// 1-based index
+function getUnicodeValidChars(): UnicodeValidChar[] {
+  const unicodeChars = fs
+    .readFileSync(`./src/UnicodeData.txt`) // http://www.unicode.org/Public/8.0.0/ucd/UnicodeData.txt
+    .toString()
+    .trim()
+    .split(/\n|\r\n/g);
+
+  let validUnicode: UnicodeValidChar[] = [];
+
+  let i = 1;
+  for (const char of unicodeChars) {
+    const glyphDescription = char.split(';')[1].split(';')[0];
+
+    if (glyphDescription !== '<control>') {
+      const hex = char.split(';')[0];
+      const glyph = String.fromCharCode(parseInt(hex, 16));
+      validUnicode[i] = { claimId: i, hex, glyphDescription, glyph };
+      i++;
+    }
   }
+  return validUnicode;
+}
 
-  return IDNonOverlappingClaim;
+function isClaimNonOverlapping(
+  fabric: string[],
+  claimDetail: ClaimDetail
+): boolean {
+  const { id, left, top, width, height } = claimDetail;
+  let claimedFabric: string[] = fabric;
+  let isClaimNonOverlapping: boolean = true;
+
+  let j = left;
+
+  let ni = top + height;
+  let nj = left + width;
+
+  for (let i = top; i < ni; ++i, j = left) {
+    for (; j < nj; ++j) {
+      // Si el espacio tiene una X entonces no es el correcto
+      if (claimedFabric[i].substr(j, 1) == 'X') {
+        isClaimNonOverlapping = false;
+      }
+    }
+  }
+  return isClaimNonOverlapping;
 }
 
 function insertClaimInFabricAlternative(
@@ -158,9 +191,10 @@ function insertClaimInFabricAlternative(
 
   for (let i = top; i < ni; ++i, j = left) {
     for (; j < nj; ++j) {
+      // Si el espacio está vacio '.'
       if (claimedFabric[i].substr(j, 1) == '.') {
         isNonOverlapping = true;
-        replaceChar = '#';
+        replaceChar = unicodeValidChars[id].glyph; //'#';
       } else {
         isNonOverlapping = false;
         replaceChar = 'X';
@@ -174,10 +208,51 @@ function insertClaimInFabricAlternative(
     }
   }
 
+  // TODO: Limpiar variable inncesaria isNonOverlapping no se puede saber si claim se
+  // sobrepone a otro al momento de inserción, se revisa al final de todas las inserciones
   return { claimedFabric, isNonOverlapping };
 }
 
+function getIDNonOverlappingClaim(
+  fabricHeight: number,
+  fabricWidth: number,
+  input: string = readInputFromFile('03')
+): number | null {
+  const claims: string[] = formatInputString(input, /;|\n|\r\n/g);
+  let IDNonOverlappingClaim: number | null = null;
+  let fabric: string[] = createFabric(fabricHeight, fabricWidth);
+  // console.log(fabric);
+
+  // Primero insertamos todos los claims (pedidos) en la fabric (tela)
+  for (const claim of claims) {
+    // console.log(claim);
+    const claimDetail: ClaimDetail = parseClaim(claim);
+    // console.log(claimDetail);
+    const { claimedFabric, isNonOverlapping } = insertClaimInFabricAlternative(
+      fabric,
+      claimDetail
+    );
+    fabric = claimedFabric;
+    // IDNonOverlappingClaim = isNonOverlapping ? claimDetail.id : null;
+    // console.log(fabric);
+  }
+
+  // Buscamos el claim que no tiene ninguna X en su area
+  for (const claim of claims) {
+    const claimDetail: ClaimDetail = parseClaim(claim);
+    // console.log(claimDetail);
+
+    if (isClaimNonOverlapping(fabric, claimDetail)) {
+      IDNonOverlappingClaim = claimDetail.id;
+      break;
+    }
+  }
+
+  return IDNonOverlappingClaim;
+}
+
 export {
+  unicodeValidChars,
   calculateOverlappingInches,
   createFabric,
   parseClaim,
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import * as main from './03';
 const result = main.getIDNonOverlappingClaim(
   1000,
   1000
+  // 9,
+  // 11,
   // '#1 @ 1,3: 4x4; #2 @ 3,1: 4x4; #3 @ 5,5: 2x2'
 );
 console.log(`The result is ${result}`);
