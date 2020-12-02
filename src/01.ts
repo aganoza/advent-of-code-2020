6e433402152cf95162dade09a637792cb28e08ec
aganoza <aganoza@users.noreply.github.com>
2020-12-01 20:48:03 -0500

    1

--- src/01.ts
+++ src/01.ts
@@ -1,18 +1,100 @@
-import { readInputFromFile, formatInputString } from './utils';
+import { readInputFromFile, formatInputString } from "./utils";
 
-function wat(input: string = readInputFromFile('01')): number {
+function getExpenseReport(input: string = readInputFromFile("01")): number {
   // console.log(`The input is: ${input}`);
   const inputs: string[] = formatInputString(input);
-  // console.log(`The inputs are: ${inputs}`);
-  let frequency: number = 0;
+  const entries: number[] = inputs.map((input) => parseInt(input, 10));
+
+  // console.log(`The inputs are: ${entries}`);
+  // console.log({ entries });
+
+  let band: number = 0;
 
   let i = 0;
-  for (const input of inputs) {
-    // console.log(`Round #${i}`);
-    // console.log(`-> Current frecuency is ${frequency}`);
+  let j = 0;
+
+  outer: for (i = 0; i < entries.length; i++) {
+    for (j = 0; j < entries.length; j++) {
+      // console.log(`i: ${i} - j: ${j}`);
+      // console.log(
+      //   `${entries[i]} y ${entries[j]} es ${
+      //     entries[i] + entries[j]
+      //   } es ${entries[i] + entries[j] === 2020}`
+      // );
+
+      if (entries[i] + entries[j] === 2020) {
+        // console.log("---------------------entré");
+        band = 1;
+        break outer;
+      }
+    }
   }
 
-  return frequency;
+  if (band === 0) {
+    i -= 1;
+    j -= 1;
+  }
+
+  // console.log(`i: ${i}`);
+  // console.log(`j: ${j}`);
+  // console.log(
+  //   `Se encontró entries: ${entries[i]} y ${entries[j]} suman ${
+  //     entries[i] + entries[j]
+  //   }`
+  // );
+
+  return entries[i] * entries[j];
 }
 
-export { wat };
+function getExpenseReportOfThree(
+  input: string = readInputFromFile("01")
+): number {
+  // console.log(`The input is: ${input}`);
+  const inputs: string[] = formatInputString(input);
+  const entries: number[] = inputs.map((input) => parseInt(input, 10));
+
+  // console.log(`The inputs are: ${entries}`);
+  // console.log({ entries });
+
+  let band: number = 0;
+
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  outer: for (i = 0; i < entries.length; i++) {
+    for (j = 0; j < entries.length; j++) {
+      for (k = 0; k < entries.length; k++) {
+        // console.log(`i: ${i} - j: ${j}`);
+        // console.log(
+        //   `${entries[i]} y ${entries[j]} es ${
+        //     entries[i] + entries[j]
+        //   } es ${entries[i] + entries[j] === 2020}`
+        // );
+
+        if (entries[i] + entries[j] + entries[k] === 2020) {
+          // console.log("---------------------entré");
+          band = 1;
+          break outer;
+        }
+      }
+    }
+  }
+
+  if (band === 0) {
+    i -= 1;
+    j -= 1;
+  }
+
+  // console.log(`i: ${i}`);
+  // console.log(`j: ${j}`);
+  // console.log(
+  //   `Se encontró entries: ${entries[i]} y ${entries[j]} suman ${
+  //     entries[i] + entries[j]
+  //   }`
+  // );
+
+  return entries[i] * entries[j] * entries[k];
+}
+
+export { getExpenseReport, getExpenseReportOfThree };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import * as main from './01';
+import * as main from "./01";
 
-const result = main.wat();
+const result = main.getExpenseReportOfThree("1721,979,366,299,675,1456");
+// const result = main.getExpenseReport("1721,979");
 console.log(`The result is ${result}`);
