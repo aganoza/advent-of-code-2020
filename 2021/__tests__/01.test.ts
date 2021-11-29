e3e654633b6f2a6144890b238eda9476908b5ffa
Tony Ganoza <aganoza@users.noreply.github.com>
2021-11-29 08:41:33 -0500

    chore: tests listos para 2021

--- 2021/__tests__/01.test.ts
+++ 2021/__tests__/01.test.ts
@@ -1,20 +1,8 @@
-import { getExpenseReport, getExpenseReportOfThree } from "../01";
+import { wat } from "../00";
 import { readInputFromFile } from "../utils";
 
-const listOfEntries: [string, number][] = [
-  ["1721,979,366,299,675,1456", 514579],
-  [readInputFromFile("01"), 485739],
-];
+const listOfEntries: [string, number][] = [[readInputFromFile("00"), 0]];
 
 test.each(listOfEntries)("%s results in %i", (entries, expected) => {
-  expect(getExpenseReport(entries)).toBe(expected);
-});
-
-const listOfEntries2: [string, number][] = [
-  ["1721,979,366,299,675,1456", 241861950],
-  [readInputFromFile("01"), 161109702],
-];
-
-test.each(listOfEntries2)("%s results in %i", (entries, expected) => {
-  expect(getExpenseReportOfThree(entries)).toBe(expected);
+  expect(wat(entries)).toBe(expected);
 });
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/2018/"],
+  testPathIgnorePatterns: ["/2018/", "/2020/"],
 };
