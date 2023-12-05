bda662a25e90a2be865eb6f167a6b70be675218b
Tony Ganoza <aganoza@users.noreply.github.com>
2023-12-05 17:06:27 -0500

    5.2 solo números pequeños

--- 2023/05.ts
+++ 2023/05.ts
@@ -1,5 +1,27 @@
 import { readInputFromFile, formatInputStringComplete } from "./utils";
 
+function getLocationFromSeed(seed: number, mapping: string[]): number {
+  // console.log({ mapping });
+  for (const line of mapping) {
+    // console.log({ line });
+    const [destination, source, range] = line
+      .split(/\s/g)
+      .map((x) => parseInt(x));
+
+    if (source <= seed && seed < source + range) {
+      // console.log({
+      //   destination,
+      //   source,
+      //   range,
+      //   newSeed: destination + (seed - source),
+      // });
+      return destination + (seed - source);
+    }
+  }
+  // console.log({ destination, source, range, seed });
+  return seed;
+}
+
 function getLowestLocationOfSeedNumbers(
   input: string = readInputFromFile("05")
 ): number {
@@ -10,28 +32,6 @@ function getLowestLocationOfSeedNumbers(
 
   const [first, ...mappings] = inputs;
   const seeds = first.match(/\d+/g) ?? [];
-
-  function getLocationFromSeed(seed: number, mapping: string[]): number {
-    // console.log({ mapping });
-    for (const line of mapping) {
-      // console.log({ line });
-      const [destination, source, range] = line
-        .split(/\s/g)
-        .map((x) => parseInt(x));
-
-      if (source <= seed && seed < source + range) {
-        // console.log({
-        //   destination,
-        //   source,
-        //   range,
-        //   newSeed: destination + (seed - source),
-        // });
-        return destination + (seed - source);
-      }
-    }
-    // console.log({ destination, source, range, seed });
-    return seed;
-  }
 
   let locations: number[] = [];
   for (const seedStr of seeds) {
@@ -50,4 +50,47 @@ function getLowestLocationOfSeedNumbers(
   return result;
 }
 
-export { getLowestLocationOfSeedNumbers };
+function getLowestLocationOfSeedNumbersWithRange(
+  input: string = readInputFromFile("05")
+): number {
+  const inputs: string[] = formatInputStringComplete(input, /\n\n|\r\n\r\n/g);
+  // console.log(inputs);
+
+  let result: number = 0;
+
+  const [first, ...mappings] = inputs;
+  const seeds: string[] = first.match(/\d+/g) ?? [];
+
+  let newSeeds: Set<number> = new Set();
+
+  for (let i = 0; i < seeds.length; i++) {
+    if (i % 2 === 0) {
+      const source = parseInt(seeds[i]);
+      const range = parseInt(seeds[i + 1]);
+      // console.log({ i, source, range, seeds });
+      for (let j = 0; j < range; j++) {
+        newSeeds.add(source + j);
+      }
+      // console.dir(newSeeds, { depth: 6 });
+    }
+  }
+
+  let locations: number[] = [];
+  for (let seed of newSeeds) {
+    for (const line of mappings) {
+      const [, ...mapping] = line.split(/\n/g);
+      seed = getLocationFromSeed(seed, mapping);
+    }
+    locations.push(seed);
+  }
+
+  result = Math.min(...locations);
+  // console.log({ seeds, mappings, locations, result });
+
+  return result;
+}
+
+export {
+  getLowestLocationOfSeedNumbers,
+  getLowestLocationOfSeedNumbersWithRange,
+};
--- 2023/index.ts
+++ 2023/index.ts
@@ -1,5 +1,5 @@
 import * as main from "./05";
 
-const result = main.getLowestLocationOfSeedNumbers();
+const result = main.getLowestLocationOfSeedNumbersWithRange();
 
 console.log(`El resultado es ${result}`);
