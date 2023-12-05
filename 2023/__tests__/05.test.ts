ed618001430d621ba12cd5aeb9965b17be6fd1cb
Tony Ganoza <aganoza@users.noreply.github.com>
2023-12-05 18:14:37 -0500

    5 tests

--- 2023/__tests__/05.test.ts
+++ 2023/__tests__/05.test.ts
@@ -4,49 +4,49 @@ import {
 } from "../05";
 import { readInputFromFile } from "../utils";
 
-// const listOfEntries: [number, string][] = [
-//   [
-//     35,
-//     `seeds: 79 14 55 13
+const listOfEntries: [number, string][] = [
+  [
+    35,
+    `seeds: 79 14 55 13
 
-// seed-to-soil map:
-// 50 98 2
-// 52 50 48
+seed-to-soil map:
+50 98 2
+52 50 48
 
-// soil-to-fertilizer map:
-// 0 15 37
-// 37 52 2
-// 39 0 15
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
 
-// fertilizer-to-water map:
-// 49 53 8
-// 0 11 42
-// 42 0 7
-// 57 7 4
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
 
-// water-to-light map:
-// 88 18 7
-// 18 25 70
+water-to-light map:
+88 18 7
+18 25 70
 
-// light-to-temperature map:
-// 45 77 23
-// 81 45 19
-// 68 64 13
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
 
-// temperature-to-humidity map:
-// 0 69 1
-// 1 0 69
+temperature-to-humidity map:
+0 69 1
+1 0 69
 
-// humidity-to-location map:
-// 60 56 37
-// 56 93 4`,
-//   ],
-//   [551761867, readInputFromFile("05")],
-// ];
+humidity-to-location map:
+60 56 37
+56 93 4`,
+  ],
+  [551761867, readInputFromFile("05")],
+];
 
-// test.each(listOfEntries)("results in %i", (expected, entries) => {
-//   expect(getLowestLocationOfSeedNumbers(entries)).toBe(expected);
-// });
+test.each(listOfEntries)("results in %i", (expected, entries) => {
+  expect(getLowestLocationOfSeedNumbers(entries)).toBe(expected);
+});
 
 const listOfEntries2: [number, string][] = [
   [
@@ -85,7 +85,7 @@ humidity-to-location map:
 60 56 37
 56 93 4`,
   ],
-  // [551761867, readInputFromFile("05")],
+  // [57451709, readInputFromFile("05")], // TODO: Make it work for larger values
 ];
 
 test.each(listOfEntries2)("results in %i", (expected, entries) => {
