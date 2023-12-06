41336f28216d08890ac4f6855349f886821b4151
Tony Ganoza <aganoza@users.noreply.github.com>
2023-12-06 15:26:58 -0500

    typecheck & lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,34 @@
 module.exports = {
   root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    // https://github.com/typescript-eslint/typescript-eslint/blob/055add01299eb91c87323677038b5f7d08b448d4/packages/eslint-plugin/src/configs/recommended.ts
+    "eslint:recommended",
+    // https://github.com/typescript-eslint/typescript-eslint/blob/055add01299eb91c87323677038b5f7d08b448d4/packages/eslint-plugin/src/configs/eslint-recommended.ts
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+  ],
   overrides: [
     {
-      env: {
-        node: true,
-      },
-      files: ["**/*.{ts,tsx}"],
-      parser: "@typescript-eslint/parser",
-      plugins: ["jest", "@typescript-eslint"],
-      extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        // https://github.com/typescript-eslint/typescript-eslint/blob/055add01299eb91c87323677038b5f7d08b448d4/packages/eslint-plugin/src/configs/eslint-recommended.ts
-        // https://github.com/typescript-eslint/typescript-eslint/blob/055add01299eb91c87323677038b5f7d08b448d4/packages/eslint-plugin/src/configs/recommended.ts
-        "plugin:jest/recommended",
-      ],
-      rules: {
-        "@typescript-eslint/no-inferrable-types": "off",
-        "prefer-const": "off",
-        "jest/no-identical-title": "off",
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
       },
     },
   ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["jest", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-inferrable-types": "off",
+    "prefer-const": "off",
+    "jest/no-identical-title": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+  },
 };
